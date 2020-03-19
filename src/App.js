9b0663a959aedb99e78111e910b2548864f5dd70
Martin Achev <achevmd@gmail.com>
2020-03-19 14:38:06 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import particlesjsConfig from './particlesjs-config.json';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles
+          width="100vw"
+          height="100vh"
+          params={particlesjsConfig} />
+        <h1 className="martin">martin</h1>
+        <h1 className="achev">achev</h1>
+      </div>
+    );
+  }
 }
-
-export default App;
