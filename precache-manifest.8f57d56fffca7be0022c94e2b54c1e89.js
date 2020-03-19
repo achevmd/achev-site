746db42c4b8770cadbb5604bf3494449b99d86cf
Martin Achev <achevmd@gmail.com>
2020-03-19 14:55:10 +0200

    Updates

---
+++ precache-manifest.8f57d56fffca7be0022c94e2b54c1e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dafda27c123d962aa19d47aa64de9b06",
+    "url": "/achev-site/index.html"
+  },
+  {
+    "revision": "54408a8048863560bbd7",
+    "url": "/achev-site/static/css/main.2d917618.chunk.css"
+  },
+  {
+    "revision": "3b3303feb0c66c19c3d4",
+    "url": "/achev-site/static/js/2.288061d4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/achev-site/static/js/2.288061d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54408a8048863560bbd7",
+    "url": "/achev-site/static/js/main.23269ff0.chunk.js"
+  },
+  {
+    "revision": "e8ef955cdc23fa440a49",
+    "url": "/achev-site/static/js/runtime-main.dca6aed8.js"
+  }
+]);
